4d0c4914a42e8cb07d4dc28bd5e7461f7e336d50
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-08-26 15:15:40 +0530

    Implements  Scrapper function

--- scrapper/scrappy.go
+++ scrapper/scrappy.go
@@ -1,7 +1,9 @@
 package scrapper
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 
 	"github.com/gocolly/colly"
 )
@@ -9,27 +11,47 @@ import (
 type quote struct {
 	Text   string `json:"text"`
 	Author string `json:"author"`
-	//Tags   Tags `json:"tags"`
+	Tags   []tags `json:"tags"`
+}
+type tags struct {
+	Tag string `json:"tag"`
 }
 
-func scrapper(url string) {
-	quotes := &quote{}
+type quotes []quote
+
+func Scrapper() ([]byte, error) {
+	var allquotes quotes
+
 	c := colly.NewCollector(
 		colly.AllowedDomains("quotes.toscrape.com"),
 	)
-
-	c.OnHTML(".text", func(e *colly.HTMLElement) {
-		quotes.Text = e.Text
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("Something went wrong: %v error Occured", err)
 	})
-	c.OnHTML(".author", func(e *colly.HTMLElement) {
-		quotes.Author = e.Text
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Making request to URL: %v", r.URL)
+	})
+
+	c.OnHTML(".quote", func(e *colly.HTMLElement) {
+		text := e.ChildText(".text")
+		author := e.ChildText(".author")
+		var atags []tags
+		e.ForEach(".tag", func(i int, h *colly.HTMLElement) {
+			tag := h.Text
+			atags = append(atags, tags{tag})
+
+		})
+		allquotes = append(allquotes, quote{Text: text, Author: author, Tags: atags})
+	})
+	c.OnScraped(func(r *colly.Response) {
+		out, _ := json.Marshal(allquotes)
+		fmt.Println(string(out))
 	})
 	c.OnResponse(func(r *colly.Response) {
-		fmt.Printf("visited %v", r.Request.URL)
+		fmt.Printf("Successfully Visited: %v Status code: %v", r.Request.URL, r.StatusCode)
 	})
-	// c.OnHTML(".tags",func(h *colly.HTMLElement) {
-	// 	tags := Tags
-	// })
-	c.Visit("https://quotes.toscrape.com/")
 
+	c.Visit("https://quotes.toscrape.com/")
+	
+	return json.Marshal(allquotes)
 }
