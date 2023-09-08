c01c653e234b4e8b3b8448035c6240eebd2a1cb6
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-09-08 19:28:17 +0530

    This adds author details printing.

--- scrapper/scrappy.go
+++ scrapper/scrappy.go
@@ -13,13 +13,21 @@ type quote struct {
 	Author string `json:"author"`
 	Tags   []tags `json:"tags"`
 }
+
+type authordet struct {
+	Author      string `json:"author"`
+	DOB         string `json:"dob"`
+	Location    string `json:"location"`
+	Description string `json:"description"`
+}
+
 type tags struct {
 	Tag string `json:"tag"`
 }
 
 type quotes []quote
 
-func Scrapper(pathURL string) ([]byte, error) {
+func scrapQuote(pathURL string) ([]byte, error) {
 	var allquotes quotes
 
 	c := colly.NewCollector(
@@ -44,16 +52,68 @@ func Scrapper(pathURL string) ([]byte, error) {
 		})
 		allquotes = append(allquotes, quote{Text: text, Author: author, Tags: atags})
 	})
-	//Implements Author Details Scrap Logic.
+
 	c.OnScraped(func(r *colly.Response) {
 		out, _ := json.Marshal(allquotes)
 		fmt.Println(string(out))
+
 	})
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Printf("\nSuccessfully Visited: %v Status code: %v\n", r.Request.URL, r.StatusCode)
 	})
 
-	c.Visit("https://quotes.toscrape.com" + pathURL)
+	c.Visit("https://quotes.toscrape.com/" + pathURL)
 
 	return json.Marshal(allquotes)
+}
+
+func ScrapAuthorDet(authorName string) ([]byte, error) {
+	var detAuthor authordet
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("quotes.toscrape.com"),
+	)
+	c.OnError(func(r *colly.Response, err error) {
+		log.Printf("\nSomething went wrong: %v error Occured\n", err)
+	})
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("\nMaking request to URL: %v\n", r.URL)
+	})
+
+	//Implements Author Details Scrap Logic.
+
+	c.OnHTML(".author-details", func(h *colly.HTMLElement) {
+		authorName := h.ChildText(".author-title")
+		authorDesc := h.ChildText(".author-description")
+
+		authorBdate := h.ChildText(".author-born-date")
+		authorLocation := h.ChildText(".author-born-location")
+
+		detAuthor = authordet{Author: authorName, Description: authorDesc, DOB: authorBdate, Location: authorLocation}
+
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		out, _ := json.Marshal(detAuthor)
+		fmt.Println(string(out))
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Printf("\nSuccessfully Visited: %v Status code: %v\n", r.Request.URL, r.StatusCode)
+	})
+
+	c.Visit("https://quotes.toscrape.com/author/" + authorName)
+
+	return json.Marshal(detAuthor)
+
+}
+
+func ScrapQuotePage(page int) {
+	pathURL := fmt.Sprintf("page/%d", page)
+	scrapQuote(pathURL)
+}
+
+func ScrapQuoteTag(tag string) {
+	pathURL := fmt.Sprintf("tag/%s", tag)
+	scrapQuote(pathURL)
 }
